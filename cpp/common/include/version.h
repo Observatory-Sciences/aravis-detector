6c9c26b07b27adc9353fe0649720a3ac310eb3b7
George Stefan Chira <gsc@observatorysciences.co.uk>
2024-03-04 16:19:19 +0000

    Added bse files and structure
    Created a similar folder structure to odin-data.
    Added an empty cmake file, as well as version.h file (copied from odin)
    
    Created the header base header file for the plugin and added some basic
    functions that log a message to the console when used. Also added lib
    file.

---
+++ cpp/common/include/version.h
@@ -0,0 +1,18 @@
+/**
+ * @file version.h
+ * @brief 
+ * @date 2024-03-04
+ * 
+ * Define version values from git tags
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define ODIN_DATA_VERSION_MAJOR @VERSION_MAJOR@
+#define ODIN_DATA_VERSION_MINOR @VERSION_MINOR@
+#define ODIN_DATA_VERSION_PATCH @VERSION_PATCH@
+#define ODIN_DATA_VERSION_STR_SHORT "@VERSION_SHORT@"
+#define ODIN_DATA_VERSION_STR "@VERSION_STR@"
+
+#endif //VERSION_H
