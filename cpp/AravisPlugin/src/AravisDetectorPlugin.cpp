4c73acefa003711806e0a292692b229b7dadd3af
George Stefan Chira <gsc@observatorysciences.co.uk>
2024-03-06 13:04:33 +0000

    Version bug fix
    Renamed the version tgs to avoid conflicts with Odin versioning

--- cpp/AravisPlugin/src/AravisDetectorPlugin.cpp
+++ cpp/AravisPlugin/src/AravisDetectorPlugin.cpp
@@ -67,26 +67,26 @@ void AravisDetectorPlugin::configure(OdinData::IpcMessage& config, OdinData::Ipc
 
 int AravisDetectorPlugin::get_version_major()
 {
-  return ODIN_DATA_VERSION_MAJOR;
+  return ARAVIS_DETECTOR_VERSION_MAJOR;
 }
 
 int AravisDetectorPlugin::get_version_minor()
 {
-  return ODIN_DATA_VERSION_MINOR;
+  return ARAVIS_DETECTOR_VERSION_MINOR;
 }
 
 int AravisDetectorPlugin::get_version_patch()
 {
-  return ODIN_DATA_VERSION_PATCH;
+  return ARAVIS_DETECTOR_VERSION_PATCH;
 }
 
 std::string AravisDetectorPlugin::get_version_short()
 {
-  return ODIN_DATA_VERSION_STR_SHORT;
+  return ARAVIS_DETECTOR_VERSION_STR_SHORT;
 }
 
 std::string AravisDetectorPlugin::get_version_long()
 {
-  return ODIN_DATA_VERSION_STR;
+  return ARAVIS_DETECTOR_VERSION_STR;
 }
 }
