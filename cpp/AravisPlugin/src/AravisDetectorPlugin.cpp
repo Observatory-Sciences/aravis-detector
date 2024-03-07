5442bf6c87f590ebc08bb1616d000a4e2b58f6f6
George Stefan Chira <gsc@observatorysciences.co.uk>
2024-03-07 16:00:20 +0000

    Bug fix Plugin no longer crashes the Processor when it can't connect to camera

--- cpp/AravisPlugin/src/AravisDetectorPlugin.cpp
+++ cpp/AravisPlugin/src/AravisDetectorPlugin.cpp
@@ -9,7 +9,6 @@
  * @copyright Copyright (c) 2024
  * 
  */
-
 #include "AravisDetectorPlugin.h"
 #include "version.h"
 #include <boost/algorithm/string.hpp>
@@ -23,7 +22,7 @@ namespace FrameProcessor
 
 /**
  * @brief Construct for the plugin
- * Currently it only displays a message when the plugin is loaded
+ * Connects to the first available camera, or it crashes.
  */
 AravisDetectorPlugin::AravisDetectorPlugin()
 {
@@ -34,13 +33,12 @@ AravisDetectorPlugin::AravisDetectorPlugin()
     camera =  arv_camera_new(NULL, &error);
     if (ARV_IS_CAMERA (camera)) {
       printf("Found camera '%s'\n", arv_camera_get_model_name (camera, NULL));
-    }else{throw(error);}
+    }else{
+      printf("Error: \n '%s' \n Encountered when connecting to first available camera \n", error->message);}
   }
   catch(GError error){
     LOG4CXX_ERROR(logger_, "Aravis camera not connected");
-    }
-  
-
+  }
 
   logger_ = Logger::getLogger("FP.AravisDetectorPlugin");
   LOG4CXX_INFO(logger_, "AravisDetectorPlugin loaded");
