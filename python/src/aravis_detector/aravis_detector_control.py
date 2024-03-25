01f5e7b96494ee7458a82defb432e9b4e79f9d7b
Alan Greer <ajg@observatorysciences.co.uk>
2024-03-25 11:05:54 +0000

    Initial commit of the Odin control software and GUI.

---
+++ python/src/aravis_detector/aravis_detector_control.py
@@ -0,0 +1,106 @@
+import json
+import logging
+import threading
+import time
+
+from odin.adapters.parameter_tree import ParameterTree
+from odin.adapters.adapter import ApiAdapterRequest
+
+class AravisDetectorControl(object):
+    def __init__(self):
+        # Underlying FP adapter for controlling the Aravis
+        self._fp = None
+
+        self._running = True
+
+        # Camera cached parameters
+        self._camera_id = ""
+        self._acquisition_mode = ""
+        self._frame_rate = 0.0
+        self._frame_count = 0
+        self._exposure_time = 0.0
+        self._pixel_format = None
+        self._payload_bytes = 0
+        self._streaming = False
+
+        # Setup the parameter tree
+        self._parameter_tree = ParameterTree(self.create_parameter_tree())
+
+        # Start up that status and config read thread
+        self._update_thread = threading.Thread(target=self.update_loop)
+        self._update_thread.start()
+
+    def create_parameter_tree(self):
+        params = {
+            "config": {
+                "mode": (lambda: self._acquisition_mode, self.set_mode),
+                "frame_count": (lambda: self._frame_count, self.set_frame_count),
+                "frame_rate": (lambda: self._frame_rate, None),
+                "exposure_time": (lambda: self._exposure_time, self.set_exposure_time),
+                "pixel_format": (lambda: self._pixel_format, None),
+                "start_acquisition": (lambda: 0, self.start_acquisition),
+                "stop_acquisition": (lambda: 0, self.stop_acquisition)
+            },
+            "status" : {
+                "camera_id": (lambda: self._camera_id, None),
+                "streaming": (lambda: self._streaming, None),
+                "payload_bytes": (lambda: self._payload_bytes, None)
+            }
+        }
+        return params
+
+    def get(self, path, remove_root=False):
+        result = self._parameter_tree.get(path, with_metadata=True)
+        return result
+
+    def set(self, path, value):
+        self._parameter_tree.set(path, value)
+
+    def register_fp_adapter(self, fp):
+        self._fp = fp
+
+    def set_mode(self, mode):
+        logging.error("Setting mode to: {}".format(mode))
+        req = ApiAdapterRequest(data=json.dumps({"acquisition_mode": mode}))
+        self._fp.put("config/aravis", req)
+
+    def set_frame_count(self, frame_count):
+        req = ApiAdapterRequest(data=json.dumps({"frame_count": frame_count}))
+        self._fp.put("config/aravis", req)
+
+    def set_exposure_time(self, exposure_time):
+        req = ApiAdapterRequest(data=json.dumps({"exposure_time": exposure_time}))
+        self._fp.put("config/aravis", req)
+
+    def start_acquisition(self, start_flag):
+        logging.error("Starting acquisition: {}".format(start_flag))
+        req = ApiAdapterRequest(data=json.dumps({"start": start_flag}))
+        self._fp.put("config/aravis", req)
+
+    def stop_acquisition(self, stop_flag):
+        logging.error("Stopping acquisition: {}".format(stop_flag))
+        req = ApiAdapterRequest(data=json.dumps({"stop": stop_flag}))
+        self._fp.put("config/aravis", req)
+
+    def read_status(self):
+        req = ApiAdapterRequest(data=json.dumps({}))
+        config = self._fp.get("config/aravis", req).data
+        status = self._fp.get("status/aravis", req).data
+        logging.error("Config: {}".format(config))
+        logging.error("Status: {}".format(status))
+        self._camera_id = status['value'][0]['camera_id']
+        self._acquisition_mode = config['value'][0]['acquisition_mode']
+        self._frame_rate = config['value'][0]['frame_rate']
+        self._exposure_time = config['value'][0]['exposure_time']
+        self._pixel_format = config['value'][0]['pixel_format']
+        self._payload_bytes = status['value'][0]['payload']
+        self._streaming = status['value'][0]['streaming']
+
+    def update_loop(self):
+        while self._running:
+            time.sleep(1.0)
+            if self._fp is not None:
+                self.read_status()
+
+    def cleanup(self):
+        self._running = False
